47f1d65c48a1d7b215e2d6eb0b0af660897e59cc
Anirudh Nayak <anirudhnayak1508@gmail.com>
2025-04-05 22:54:35 +0530

    new habit db setup done

--- homepage/addinghabit.js
+++ homepage/addinghabit.js
@@ -3,15 +3,27 @@ const homerouter=require('./homepage/home')
 const {connecttoDB,getDB}=require("./db/connection")
 
 const router=express.Router()
-router.post('/habit',(req,res)=> {
-let habit=req.body
 
-db=getDB();
-db.collection('habits')
-.insertOne(habit)
-.then((result) => {
-   res.status(200).json(result)
+connecttoDB((err)=>  {
+  if(!err) {
+    router.post('/createhabit',(req,res)=> {
+
+      db=getDB();
+        Habitschema.create(req.body)
+        .then((data)=> res.json(data))
+        .catch((e) => res.json(e))
+
+      })
+      app.listen(PORT,() => {
+        console.log(`listening to ${PORT}`)
+      }) 
+    }
+    else {
+      res.status(500).json({error:"An  internal error  occured"})
+     }
 })
-.catch((e) => {
-  res.status(500).console.log(e)})
-})
+
+
+
+
+
