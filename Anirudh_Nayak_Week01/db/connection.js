4d3a1c73531501300d6effc7d33167554a0a348f
Anirudh Nayak <anirudhnayak1508@gmail.com>
2025-05-07 17:10:39 +0530

    connection

--- Anirudh_Nayak_Week01/db/connection.js
+++ Anirudh_Nayak_Week01/db/connection.js
@@ -1,30 +1,34 @@
+const { MongoClient } = require('mongodb');
 const mongoose=require('mongoose')
 mongoose.set('strictQuery', true);
 const path=require('path')
 require('dotenv').config({
   path: path.resolve(__dirname, '../.env')
 });
-
+let dbconnection;
 let dbuser=process.env.DB_USER
-let dbpw = encodeURIComponent(process.env.DB_PW);
+let dbpw = process.env.DB_PW;
 let cluster=process.env.DB_CLUSTER
 let dbname=process.env.DBNAME
 
 
 let uri=`mongodb+srv://${dbuser}:${dbpw}@${cluster}.mongodb.net/?retryWrites=true&w=majority&appName=${dbname}`
 
-let connecttoDB= (cb)=> mongoose.connect(uri)
-.then(()=> {
+let connecttoDB= (cb)=>{
+
+MongoClient.connect(uri)
+.then((client)=> {
   console.log("mongo db connected")
+  dbconnection=client.db()
   return cb()
 })
 .catch((e)=>  {
   console.log(e)
   return cb(e)
 })
-
+}
 const getDB=()=> {
-  return mongoose.connection.db
+  return dbconnection
 }
 
 module.exports={
