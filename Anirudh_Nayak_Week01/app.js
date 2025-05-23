b03fe64bccd13fac12a6f9f2131d96bf4be75cab
Anirudh Nayak <anirudhnayak1508@gmail.com>
2025-05-23 14:58:09 +0530

    notification works

--- Anirudh_Nayak_Week01/app.js
+++ Anirudh_Nayak_Week01/app.js
@@ -142,9 +142,11 @@ app.post('/notify-completion', passport.authenticate('jwt', { session: false }),
       console.log("After pushing notification:", user.notifications.length);
       await user.save();
       console.log("✅ Notification saved to DB");
+
     }
 
-    res.status(200).json({ success: true });
+    res.status(200).json({ success: true, message: 'Notification added' });
+
   } catch (e) {
     console.error("Notification error", e);
     res.status(500).json({ success: false, message: e.message });
